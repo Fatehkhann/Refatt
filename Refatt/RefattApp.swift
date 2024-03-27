686b6b4cf65f4852de1006bd74f79d02a58ef344
Fateh Khan <fatehkhann@gmail.com>
2024-03-27 12:01:37 +0500

    Initial Commit

---
+++ Refatt/RefattApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefattApp.swift
+//  Refatt
+//
+//  Created by Fateh Khan on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RefattApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
